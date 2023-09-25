d88ef67b595ccd0a7f889b0c9e1ecd1fbd38d3c7
Elly <dev@keiau.space>
2023-09-25 19:09:05 +0800

    Added functionality to manage images and captions
    This commit introduces a series of methods in the main file to manage images and their respective captions. Also included are regex checks for file types and error messages when a related caption or image does not exist.
    
    Changes were also made to the entities file where Image and Caption structs were modified to include extension and directory attributes. The Content and NumberOfTags attributes are currently commented out, but might be used in future iterations.

--- entities.go
+++ entities.go
@@ -1,18 +1,22 @@
 package main
 
 type Image struct {
-	Filename string
-	Filesize int64
-	Content  []byte
+	Filename  string
+	Extension string
+	Directory string
+	//Filesize int64
+	//Content  []byte
+	Caption Caption
 }
 
 type Caption struct {
-	Filename     string
-	Content      []byte
-	NumberOfTags int
+	Filename  string
+	Extension string
+	Directory string
+	//Content      []byte
+	//NumberOfTags int
 }
 
 type DataSet struct {
-	Images   map[string]Image
-	Captions map[string]Caption
+	Images map[string]Image
 }
--- main.go
+++ main.go
@@ -1,7 +1,116 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+var imageDirectory string
+var captionDirectory string
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Read all the filenames of image files in the image directory
+
+	data := DataSet{
+		Images: make(map[string]Image),
+	}
+
+	data.WriteFiles()
+	fmt.Printf("Images: %v\n", data.Images)
+	data.WriteFiles()
+	fmt.Printf("Images: %v\n", data.Images)
+	//data.CheckIfCaptionsExist()
+}
+
+func (data *DataSet) CheckIfCaptionsExist() {
+	for _, image := range data.Images {
+		if image.Caption.Filename != "" {
+			continue
+		}
+		fmt.Println("Caption for image", image.Filename, "does not exist")
+	}
+}
+
+func (data *DataSet) WriteFiles() {
+	regex := `(?i)\.(jpe?g|png|gif|bmp|txt)$`
+	var directoryToRead string
+	fmt.Print("Enter the directory to read: ")
+	_, _ = fmt.Scanln(&directoryToRead)
+	if directoryToRead == "" {
+		directoryToRead = "."
+	}
+
+	dirEntry, _ := os.ReadDir(directoryToRead)
+	for _, entry := range dirEntry {
+		t := entry.Type()
+		fmt.Println(t.String())
+		if entry.IsDir() {
+			continue
+		}
+		currentEntry := entry.Name()
+		extRegex, _ := regexp.Compile(regex)
+		extension := extRegex.FindString(currentEntry)
+		fileName := currentEntry[:len(currentEntry)-len(extension)]
+		if !extRegex.MatchString(currentEntry) {
+			continue
+		}
+		// check extension
+
+		if extension == ".txt" {
+			// check if image exists
+			if _, ok := data.Images[fileName]; !ok {
+				fmt.Println("Image file for caption", currentEntry, "does not exist")
+			}
+			if img, ok := data.Images[fileName]; ok {
+				fmt.Println("Appending the caption file:", currentEntry, "to the image file:", fileName)
+				img.Caption = Caption{Filename: currentEntry, Extension: extension}
+				data.Images[fileName] = img
+			}
+			continue
+		}
+
+		data.Images[fileName] = Image{Filename: currentEntry, Caption: Caption{}}
+
+	}
+}
+
+func (data *DataSet) WriteImages() {
+	fmt.Print("Enter the path to the image directory: ")
+	_, _ = fmt.Scanln(&imageDirectory)
+	if imageDirectory == "" {
+		imageDirectory = "."
+	}
+
+	dirEntry, _ := os.ReadDir(imageDirectory)
+	for _, entry := range dirEntry {
+		filename := entry.Name()
+		// check if an image file
+		extRegex, _ := regexp.Compile(`(?i)\.(jpe?g|png|gif|bmp)$`)
+		if !extRegex.MatchString(filename) || entry.IsDir() {
+			continue
+		}
+		data.Images[filename] = Image{Filename: filename}
+	}
+}
+
+func (data *DataSet) WriteCaptions() {
+	fmt.Print("Enter the path to the caption directory: ")
+	_, _ = fmt.Scanln(&imageDirectory)
+	if imageDirectory == "" {
+		imageDirectory = "."
+	}
+
+	dirEntry, _ := os.ReadDir(imageDirectory)
+	for _, entry := range dirEntry {
+		filename := entry.Name()
+		// check if an image file
+		extRegex, _ := regexp.Compile(`(?i)\.(txt)$`)
+		if !extRegex.MatchString(entry.Name()) || entry.IsDir() {
+			continue
+		}
+		if img, ok := data.Images[filename]; ok {
+			img.Caption = Caption{Filename: filename}
+		}
+	}
 }
