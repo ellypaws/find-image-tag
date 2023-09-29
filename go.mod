3a9116790be97dd5b0c51c10a9f2020c6ba48108
Elly <dev@keiau.space>
2023-09-30 07:42:38 +0800

    Add `autocomplete.go`

--- go.mod
+++ go.mod
@@ -4,15 +4,15 @@ go 1.21
 
 require (
 	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2
-	github.com/charmbracelet/bubbles v0.16.1
-	github.com/charmbracelet/bubbletea v0.24.1
+	github.com/charmbracelet/bubbles v0.16.2-0.20230821152602-eda891258c02
+	github.com/charmbracelet/bubbletea v0.24.2
 	github.com/charmbracelet/lipgloss v0.7.1
 	github.com/nokusukun/roggy v0.1.6
 	github.com/nokusukun/stemp v0.1.5
-
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
@@ -26,10 +26,10 @@ require (
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.15.1 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.3.8 // indirect
 )
---
+++ tui/autocomplete/autocomplete.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type model struct {
+	textInput textinput.Model
+}
+
+func initialModel() model {
+	ti := textinput.New()
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current working directory: %v", err)
+	}
+
+	ti.Prompt = ""
+	ti.Placeholder = currentDir + string(os.PathSeparator)
+	ti.Cursor.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
+	ti.Focus()
+	ti.CharLimit = 200
+	ti.Width = 50
+	ti.ShowSuggestions = true
+	ti.SetSuggestions([]string{currentDir}) // Set initial suggestions to the current directory
+
+	return model{textInput: ti}
+}
+
+func getDirectories(path string) ([]string, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var directories []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			directories = append(directories, entry.Name())
+		}
+	}
+	return directories, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			fmt.Println(m.textInput.Value())
+			return m, tea.Quit
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+		// Check for directory suggestions after key input
+		path := m.textInput.Value()
+		if path == "" {
+			path = m.textInput.Placeholder
+		}
+
+		basePath := filepath.Dir(path) // Gets the path up to the last backslash
+
+		if _, err := os.Stat(basePath); err == nil {
+			directories, err := getDirectories(basePath)
+			if err == nil {
+				for i, dir := range directories {
+					directories[i] = filepath.Join(basePath, dir)
+				}
+				m.textInput.SetSuggestions(directories)
+			}
+		} else {
+			m.textInput.SetSuggestions(nil)
+		}
+	}
+
+	var cmd tea.Cmd
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"Choose a directory:\n\n%s\n\n%s\n",
+		m.textInput.View(),
+		"(tab to complete, ctrl+n/ctrl+p to cycle through suggestions, enter to select, esc to quit)",
+	)
+}
